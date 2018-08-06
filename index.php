fec84a7c4a72b8bc839f1527180fbd810ffdfc00
Benjamin Lu <benlumia007@archmage.trulyawesomeness.com>
2018-08-06 14:00:28 -0700

    add index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/*
+===========================================================================================================
+fervent - index.php
+===========================================================================================================
+This is the most generic template file in a WordPress Theme and is one of the two required files for a 
+theme (the other being style.css). The index.php template file is flexible. It can be used to include all 
+references to the header, content, widget, footer and any other pages created in WordPress. Or it can be 
+divided into modular template files, each taking on part of the workload. If you do not provide other 
+template files, WordPress may have default files or functions to perform their jobs.
+
+@package        fervent WordPress Theme
+@copyright      Copyright (C) 2018. Benjamin Lu
+@license        GNU General Public License v2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
+@author         Benjamin Lu (https://luthemes.com/)
+===========================================================================================================
+*/
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <?php wp_head(); ?>
+    </head>
+<body <?php body_class(); ?>>
+    
+    <?php wp_footer(); ?>
+</body>
+</html>
