0bf9f3f7ecb57853db6fd7e3405e3e6f771233f4
Benjamin Lu <benlumia007@gmail.com>
2018-08-07 15:40:23 -0700

    small changes

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 <?php
 /*
 ================================================================================================
-benjlu - index.php
+Fervent - index.php
 ================================================================================================
 This is the most generic template file in a WordPress theme and is one of the two required files 
 for a theme (the other being style.css). The index.php template file is flexible. It can be used to 
@@ -10,10 +10,11 @@ include all references to the header, content, widget, footer and any other page
 WordPress. Or it can be divided into modular template files, each taking on part of the workload. 
 If you do not provide other template files, WordPress may have default files or functions to 
 perform their jobs.
-@package        benjlu WordPress Theme
-@copyright      Copyright (C) 2017. Benjamin Lu
-@license        GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
-@author         Benjamin Lu (https://www.benjlu.com/)
+
+@package        Fervent WordPress Theme
+@copyright      Copyright (C) 2018. Benjamin Lu
+@license        GNU General Public License v2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
+@author         Benjamin Lu (https://luthemes.com/)
 ================================================================================================
 */
 ?>
--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ================================================================================================
-benjlu - index.php
+Fervent - single.php
 ================================================================================================
 This is the most generic template file in a WordPress theme and is one of the two required files 
 for a theme (the other being style.css). The index.php template file is flexible. It can be used to 
@@ -9,10 +9,11 @@ include all references to the header, content, widget, footer and any other page
 WordPress. Or it can be divided into modular template files, each taking on part of the workload. 
 If you do not provide other template files, WordPress may have default files or functions to 
 perform their jobs.
-@package        benjlu WordPress Theme
-@copyright      Copyright (C) 2017. Benjamin Lu
+
+@package        Fervent WordPress Theme
+@copyright      Copyright (C) 2018. Benjamin Lu
 @license        GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
-@author         Benjamin Lu (https://www.benjlu.com/)
+@author         Benjamin Lu (https://luthemes.com/)
 ================================================================================================
 */
 ?>
