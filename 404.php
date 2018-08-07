7b0aaeef76013b91242e6898a4bbfaeb3a2c6350
Benjamin Lu <benlumia007@gmail.com>
2018-08-07 16:24:01 -0700

    description

--- 404.php
+++ 404.php
@@ -3,12 +3,13 @@
 ================================================================================================
 fervent - index.php
 ================================================================================================
-This is the most generic template file in a WordPress theme and is one of the two required files 
-for a theme (the other being style.css). The index.php template file is flexible. It can be used to 
-include all references to the header, content, widget, footer and any other pages created in 
-WordPress. Or it can be divided into modular template files, each taking on part of the workload. 
-If you do not provide other template files, WordPress may have default files or functions to 
+This is the most generic template file in a WordPress theme and is one of the two required files
+for a theme (the other being style.css). The index.php template file is flexible. It can be used to
+include all references to the header, content, widget, footer and any other pages created in
+WordPress. Or it can be divided into modular template files, each taking on part of the workload.
+If you do not provide other template files, WordPress may have default files or functions to
 perform their jobs.
+
 @package        fervent WordPress Theme
 @copyright      Copyright (C) 2017. Benjamin Lu
 @license        GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
