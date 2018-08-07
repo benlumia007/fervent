94280722e370f79c1c4765602b43941c5242ae2c
Benjamin Lu <benlumia007@gmail.com>
2018-08-07 08:38:32 -0700

    add framework.php

---
+++ framework/framework.php
@@ -0,0 +1,29 @@
+<?php
+/*
+===========================================================================================================
+Fervent - framework.php
+===========================================================================================================
+This is the most generic template file in a WordPress theme and is one of the required files for a theme. 
+This framework.php template file allows you to add features and functionality that has been preset to be use 
+in this WordPress theme which is stored in the theme's framework directory in the theme folder.
+
+@package        Fervent WordPress Theme
+@copyright      Copyright (C) 2018. Benjamin Lu
+@license        GNU General Public License v2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
+@author         Benjamin Lu (https://luthemes.com/)
+===========================================================================================================
+*/
+
+/*
+===========================================================================================================
+Table of Content
+===========================================================================================================
+ 1.0 - Require Files
+===========================================================================================================
+*/
+
+/*
+===========================================================================================================
+ 1.0 - Require Files
+===========================================================================================================
+*/
